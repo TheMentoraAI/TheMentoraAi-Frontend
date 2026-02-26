885620fd6c7251bc701f1a0f3c4439fae485dc3e
KshitijDatir <kshitijdatir1@gmail.com>
2026-02-26 10:50:51 +0530

    1

---
+++ src/app/evaluation/page.js
@@ -0,0 +1,10 @@
+'use client';
+
+export default function EvaluationPage() {
+    return (
+        <div>
+            <h1>Evaluation</h1>
+            <p>Evaluation page coming soon.</p>
+        </div>
+    );
+}
